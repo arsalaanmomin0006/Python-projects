f1373513679064e46127bd909ee839a3c818c03d
arsalaanmomin0006 <arsalaanmomin.0006@gmail.com>
2026-06-14 17:51:44 +0530

    Uploaded first Python project

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+F_n = int(input("Enter first no:"))
+S_n = int(input("Enter second no:"))
+operation = input("Enter what operation to do(+, -, *, /):")
+if operation == "+":
+    print(F_n + S_n)
+elif operation == "-":
+    print(F_n - S_n)
+elif operation == "*":
+    print(F_n * S_n)
+elif operation == "/":
+    print(F_n / S_n)
