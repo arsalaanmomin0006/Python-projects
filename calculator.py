51cbb4083dfdaa0e58461dfa710c325951c5b236
arsalaanmomin0006 <arsalaanmomin.0006@gmail.com>
2026-06-26 21:49:22 +0530

    Added while loop so that calculations can be continued.
    Refactored calculator to allow continuous calculations until user decides to stop.

--- calculator.py
+++ calculator.py
@@ -1,11 +1,17 @@
-F_n = int(input("Enter first no:"))
-S_n = int(input("Enter second no:"))
-operation = input("Enter what operation to do(+, -, *, /):")
-if operation == "+":
-    print(F_n + S_n)
-elif operation == "-":
-    print(F_n - S_n)
-elif operation == "*":
-    print(F_n * S_n)
-elif operation == "/":
-    print(F_n / S_n)
+while True:
+    F_n = int(input("Enter first no:"))
+    S_n = int(input("Enter second no:"))
+    operation = input("Enter what operation to do(+, -, *, /):")
+
+    if operation == "+":
+        print(F_n + S_n)
+    elif operation == "-":
+        print(F_n - S_n)
+    elif operation == "*":
+        print(F_n * S_n)
+    elif operation == "/":
+        print(F_n / S_n)
+
+    A_n = input("Want to continue the calculation? Answer Yes or No:")
+    if A_n == "No":
+        break
