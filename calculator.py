1b803f52aa7ca61e32240187e56a76aa6e28661d
arsalaanmomin0006 <arsalaanmomin.0006@gmail.com>
2026-07-15 16:15:18 +0530

    Added def i.e., functions.

--- calculator.py
+++ calculator.py
@@ -1,19 +1,33 @@
+def add(a, b):
+    print(a + b)
+    
+def subtract(a, b):
+    print(a - b)
+
+def multiply(a, b):
+    print(a *b)
+
+def divide(a, b):
+    print(a / b)
+
 while True:
     F_n = int(input("Enter first no:"))
     S_n = int(input("Enter second no:"))
     operation = input("Enter what operation to do(+, -, *, /):")
 
     if operation == "+":
-        print(F_n + S_n)
+        add(F_n, S_n)
     elif operation == "-":
-        print(F_n - S_n)
+        subtract(F_n,  S_n)
     elif operation == "*":
-        print(F_n * S_n)
+        multiply(F_n, S_n)
     elif operation == "/":
         if S_n == 0:
             print("Cannot divide by zero")
         else:
-            print(F_n / S_n)
+            divide(F_n, S_n)
+    else:
+        print("Invalid Operation")
 
     A_n = input("Want to continue the calculation? Answer Yes or No:")
     if A_n == "No":
